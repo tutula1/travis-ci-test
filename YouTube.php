7d03fc5a937d6abeffe27dd5ff7c8d903b1b443f
Binh Duong <binh.duong@vingle.net>
2016-01-20 10:07:47 +0700

    update index

---
+++ YouTube.php
@@ -0,0 +1,31 @@
+<?php
+class YouTube {
+  /** * Use to get the key from YouTube url *
+  * @return string */
+ public function getKeyCode($url) {
+  $pattern = '/v=(\w+)/';
+  $matches = array();
+  preg_match($pattern, $url, $matches);
+  if (isset($matches[1])) {
+   return $matches[1];
+  }
+  return false;
+ }
+}
+
+class RemoteConnect
+{
+    public function connectToServer($serverName=null)
+    {
+        if($serverName==null){
+            throw new Exception("That's not a server name!");
+        }
+        $fp = fsockopen($serverName,80);
+        return ($fp) ? true : false;
+    }
+
+    public function returnSampleObject()
+    {
+        return $this;
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
-echo "1";
+include 'YouTube.php';
+/*class YouTubeTest extends PHPUnit_Framework_TestCase {
+ private $youTube;
+ protected function setUp() {
+  $this->youTube = new YouTube();
+ }
+ public function testCase1() {
+  $url = 'http://www.youtube.com/watch?v=_RRnyChxijA&feature=related&';
+  $keyCode = $this->youTube->getKeyCode($url);
+  $this->assertEquals('_RRnyChxijA', $keyCode);
+ }
+ public function testCase2() {
+  $url = 'http://www.youtube.com/watch?v=-WolCnOLPg8&feature=related';
+  $keyCode = $this->youTube->getKeyCode($url);
+  $this->assertEquals('-WolCnOLPg8', $keyCode);
+ }
+}*/
+class RemoteConnectTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp(){ }
+    public function tearDown(){ }
+
+    public function testConnectionIsValid()
+    {
+        // test to ensure that the object from an fsockopen is valid
+        $connObj = new RemoteConnect();
+        $serverName = 'www.google.com1';
+        $this->assertTrue($connObj->connectToServer($serverName) !== false);
+    }
+}
